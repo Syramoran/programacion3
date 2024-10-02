86831bec798cf4a623b2c12c6ff27f64d7ab6db9
Syramoran <syramoranicr@gmail.com>
2024-10-02 10:10:19 -0300

    primera version de patch que anda

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import {conexion} from './conexion.js'
+import {conexion} from './conexion.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).send({'estado':true});
@@ -44,6 +46,55 @@ app.get('/reclamos-estados/:idReclamosEstado', async (req, res) => {
         res.status(500).json({mensaje:'error interno'});
     }
 })
+
+// update
+
+app.patch('/reclamos-estados/:idReclamosEstado', async (req, res) => {
+    try{
+        
+        const { descripcion, activo } = req.body;
+        // Se está usando destructuring assignment para extraer las propiedades descripcion y activo del objeto req.body.
+        // Esta línea simplifica el acceso a esos datos en lugar de tener que escribir req.body.descripcion y req.body.activo cada vez que quieras usarlos.
+            // const descripcion = req.body.descripcion;
+            // const activo = req.body.activo;
+
+
+        // chequeo de que llegue la info necesaria
+        if(!descripcion) {
+            return res.status(404).json({
+                mensaje:"Se requiere el campo descripción"
+            })
+        }
+
+        if (activo === undefined || activo === null) {
+            return res.status(404).json({
+                mensaje:"Se requiere el campo descripción"
+            })
+        }
+
+        const idReclamosEstado = req.params.idReclamosEstado;
+
+        const sql = 'UPDATE reclamos_estado SET descripcion=?, activo=? WHERE idReclamosEstado=?';
+        const [result] = await conexion.query(sql, [descripcion, activo, idReclamosEstado]);
+
+        if (result.affectedRows === 0){
+            return res.status(404).json({
+                mensaje:'No se pudo modificar.'
+            })
+        }
+
+        res.status(200).json({
+            mensaje:'Reclamo modificado'
+        });
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({
+            mensaje:'Error interno.'
+        });
+    }
+});
+
 
 
 const puerto = process.env.PUERTO;
